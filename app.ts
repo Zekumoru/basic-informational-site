9be36db1ca77082fc073a589364206ef8ef68116
root <root@vmi1481157.contaboserver.net>
2023-12-21 22:24:40 +0100

    Add localhost env

--- app.ts
+++ app.ts
@@ -2,14 +2,14 @@ import "dotenv/config";
 import { createServer } from "node:http";
 import pageHandler from "./handlers/pageHandler";
 
-const hostname = process.env.HOSTNAME;
-const host = process.env.HOST;
+const localHostname = process.env.LOCAL_HOSTNAME;
+const localHost = process.env.LOCAL_HOST;
 const port = Number(process.env.PORT);
 
 const server = createServer(async (req, res) => {
   return await pageHandler(req, res);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Listening on ${host}:${port}`);
+server.listen(port, localHostname, () => {
+  console.log(`Listening on ${localHost}:${port}`);
 });
